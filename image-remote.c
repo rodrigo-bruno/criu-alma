e7dfd643991f58cf552bcef30a78ab0093e31b05
rbruno <rbruno@gsd.inesc-id.pt>
2015-07-15 13:20:41 +0100

    Plan B seems promising.

--- image-remote.c
+++ image-remote.c
@@ -16,41 +16,18 @@
 #include <semaphore.h>
 
 #include "criu-log.h"
-#include <utlist.h>
+#include "utlist.h"
 
 #define DEFAULT_PORT 9997
 #define DEFAULT_HOST "localhost"
 #define DEFAULT_LISTEN 50
 #define PATHLEN 32
 #define DUMP_FINISH "DUMP_FINISH"
-#define PIPE_READ 0
-#define PIPE_WRITE 1
-#define PAGESIZE 4096
-#define BUF_SIZE PAGESIZE*250
 
-// TODO - this may be problematic because of double evaluation...
-#define MIN(x, y) (((x) < (y)) ? (x) : (y))
-
-// http://pubs.opengroup.org/onlinepubs/9699919799/functions/fmemopen.html
-// http://pubs.opengroup.org/onlinepubs/9699919799/functions/open_memstream.html
-
-// TODO - maybe it would be a good idea to perform some cleanups at the end (in
-// both sides).
-
-typedef struct rbuf {
-    char buffer[BUF_SIZE];
-    int nbytes; // How many bytes are in the buffer.
-    struct rbuf *next, *prev;
-} remote_buffer;
-
-typedef struct rimg {
+typedef struct el {
     char path[PATHLEN];
     int sockfd;
-    int pipe[2]; // pipe[0] is RDONLY, pipe[1] is WRONLY
-    struct rimg *next, *prev;
-    pthread_t worker;
-    remote_buffer* buf_head;
-    
+    struct el *next, *prev;
 } remote_image;
 
 int path_cmp(remote_image *a, remote_image *b) {
@@ -58,9 +35,7 @@ int path_cmp(remote_image *a, remote_image *b) {
 }
 
 int fd_cmp(remote_image *a, remote_image *b) {
-    return  a->sockfd == b->sockfd || 
-            a->pipe[PIPE_READ] == b->pipe[PIPE_READ] || 
-            a->pipe[PIPE_WRITE] == b->pipe[PIPE_WRITE];
+    return a->sockfd == b->sockfd;
 }
 
 static remote_image *head = NULL;
@@ -88,8 +63,6 @@ int is_remote_image(int fd) {
     remote_image *result, like;
 
     like.sockfd = fd;
-    like.pipe[PIPE_READ] = fd;
-    like.pipe[PIPE_WRITE] = fd;
 
     pthread_mutex_lock(&lock);
     DL_SEARCH(head, result, &like, fd_cmp);
@@ -100,85 +73,6 @@ int is_remote_image(int fd) {
     }
     
     return 0;
-}
-
-void* buffer_remote_image(remote_image* rimg, int src_fd, int dst_fd) {
-    int n;
-    remote_buffer* curr_buf = rimg->buf_head;
-    int curr_offset = 0;
-    
-    while(1) {
-        n = read(   src_fd, 
-                    curr_buf->buffer + curr_offset, 
-                    BUF_SIZE - curr_offset);
-        if (n == 0) {
-            close(src_fd);
-            break;
-        }
-        else if (n > 0) {
-            curr_offset += n;
-            curr_buf->nbytes += n;
-            if(curr_offset == BUF_SIZE) {
-                remote_buffer* buf = malloc(sizeof (remote_buffer));
-                if(buf == NULL) {
-                    pr_perror("Unable to allocate remote_buffer structures");
-                }
-                buf->nbytes = 0;
-                DL_APPEND(rimg->buf_head, buf);
-                curr_offset = 0;
-                curr_buf = buf;
-                // TODO - make sure the list is appending at the end!
-            }
-            
-        }
-        else {
-            pr_perror("Read on %s socket failed", rimg->path);
-            return NULL;
-        }
-    }
-    
-    curr_buf = rimg->buf_head;
-    curr_offset = 0;
-    while(1) {
-        if(!curr_buf) {
-            break;
-            close(dst_fd);
-        }
-        n = write(
-                    dst_fd, 
-                    curr_buf->buffer + curr_offset, 
-                    MIN(BUF_SIZE, curr_buf->nbytes) - curr_offset);
-        if(n > -1) {
-            curr_offset += n;
-            if(curr_offset == BUF_SIZE) {
-                curr_buf = curr_buf->next;
-                curr_offset = 0;
-            }
-        }
-        else {
-             pr_perror("Write on %s socket failed (n=%d)", rimg->path, n);
-        }
-    }
-    return NULL;
-}
-
-/* Dump side:
- *  read pipe, write to buffer
- *  when pipe is closed, send buffer to socket
- */
-void* buffer_to_remote_image(void* ptr) {
-    remote_image* rimg = (remote_image*) ptr;
-    return buffer_remote_image(rimg, rimg->pipe[PIPE_READ], rimg->sockfd);;
-    
-}
-
-/* Restore side 
- *  read socket, write to buffer
- *  when socket is closed, write to pipe
- */
-void* buffer_from_remote_image(void* ptr){
-    remote_image* rimg = (remote_image*) ptr;
-    return buffer_remote_image(rimg, rimg->sockfd, rimg->pipe[PIPE_WRITE]);
 }
 
 void* accept_remote_image_connections(void* null) {
@@ -198,11 +92,6 @@ void* accept_remote_image_connections(void* null) {
         if (img == NULL) {
             pr_perror("Unable to allocate remote_image structures");
         }
-        
-        remote_buffer* buf = malloc(sizeof (remote_buffer));
-        if(buf == NULL) {
-            pr_perror("Unable to allocate remote_buffer structures");
-        }
 
         n = read(imgsockfd, img->path, PATHLEN);
         if (n < 0) {
@@ -210,33 +99,8 @@ void* accept_remote_image_connections(void* null) {
         } else if (n == 0) {
             pr_perror("Remote image socket closed before receiving path");
         }
-
-        if (!strncmp(img->path, DUMP_FINISH, sizeof (DUMP_FINISH))) {
-            pr_info("Dump side is finished!\n");
-            free(img);
-            free(buf);
-            finished = 1;
-            close(imgsockfd);
-            return NULL;
-        }
-
-        if(pipe(img->pipe)) {
-            pr_perror("Cannot create pipe from buffer remote image");
-        }
-        
         img->sockfd = imgsockfd;
-        img->buf_head = NULL;
-        buf->nbytes = 0;
-        DL_APPEND(img->buf_head, buf);
-        
-        if (pthread_create( &img->worker, 
-                            NULL, 
-                            buffer_from_remote_image, 
-                            (void*) img)) {
-                pr_perror("Unable to create socket thread");
-                return NULL;
-        } 
-        
+
         pr_info("Reveiced %s, fd = %d\n", img->path, img->sockfd);
 
         pthread_mutex_lock(&lock);
@@ -246,6 +110,17 @@ void* accept_remote_image_connections(void* null) {
         
         // <underscore> DEBUG
         check_remote_connections();
+
+        if (!strncmp(img->path, DUMP_FINISH, sizeof (DUMP_FINISH))) {
+            pr_info("Dump side is finished!\n");
+            finished = 1;
+            return NULL;
+        }
+
+        // TODO - launch aux thread to buffer data from socket?
+        // http://pubs.opengroup.org/onlinepubs/9699919799/functions/fmemopen.html
+        // http://pubs.opengroup.org/onlinepubs/9699919799/functions/open_memstream.html
+
     }
 }
 
@@ -308,6 +183,9 @@ int prepare_remote_image_connections() {
 
 int get_remote_image_connection(char* path) {
     remote_image *result, like;
+    int sockfd;
+    int error = 0;
+    socklen_t len = sizeof (error);
 
     strncpy(like.path, path, PATHLEN);
 
@@ -327,7 +205,17 @@ int get_remote_image_connection(char* path) {
         sem_wait(&semph);
     }
 
-    return result->pipe[PIPE_READ];
+    sockfd = result->sockfd;
+    pthread_mutex_lock(&lock);
+    DL_DELETE(head, result);
+    pthread_mutex_unlock(&lock);
+    free(result);
+
+    if (getsockopt(sockfd, SOL_SOCKET, SO_ERROR, &error, &len)) {
+        pr_perror("Returning invalid socket (fd = %d), error = %d", sockfd, error);
+    }
+
+    return sockfd;
 }
 
 int open_remote_image_connection(char* path) {
@@ -358,43 +246,13 @@ int open_remote_image_connection(char* path) {
         pr_perror("Unable to connect to remote restore host %s", DEFAULT_HOST);
         return -1;
     }
-   
+
     if (write(sockfd, path, PATHLEN) < 1) {
         pr_perror("Unable to send path to remote image connection");
         return -1;
     }
 
-    remote_image* img = malloc(sizeof (remote_image));
-    if (img == NULL) {
-        pr_perror("Unable to allocate remote_image structures");
-    }
-
-    remote_buffer* buf = malloc(sizeof (remote_buffer));
-    if(buf == NULL) {
-        pr_perror("Unable to allocate remote_buffer structures");
-    }
-    
-    if(pipe(img->pipe)) {
-        pr_perror("Cannot create pipe from buffer remote image");
-    }
-
-    strncpy(img->path, path, PATHLEN);
-    img->sockfd = sockfd;
-    img->buf_head = NULL;
-    buf->nbytes = 0;
-    DL_APPEND(img->buf_head, buf);
-
-    if (pthread_create( &img->worker, 
-                        NULL, 
-                        buffer_to_remote_image, 
-                        (void*) img)) {
-            pr_perror("Unable to create socket thread");
-            return -1;
-    } 
-
-    DL_APPEND(head, img);
-    
-    return img->pipe[PIPE_WRITE];
+    return sockfd;
 }
 
 int finish_remote_dump() {
@@ -404,8 +262,7 @@ int finish_remote_dump() {
         pr_perror("Unable to open finish dump connection");
         return -1;
     }
-    // <underscore> TODO - uncomment this.
-    //close(fd);
+    close(fd);
     return 0;
 }
 
